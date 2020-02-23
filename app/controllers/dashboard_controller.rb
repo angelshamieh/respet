93e8887fd9f9a5fdef4c1a4dbdc4349d26ab3301
talia wolf <taliamwolf@gmail.com>
2020-02-23 18:33:26 +0200

    added dashboard and settings page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,8 @@ class DashboardController < ApplicationController
   def profile
     @user = current_user
   end
+
+  def settings
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
-Rails.application.routes do
+Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :animals, except: :destroy
 
   get 'dashboard', to: 'dashboard#profile', as: :dashboard
+  get 'settings', to: 'dashboard#settings', as: :settings
 end
