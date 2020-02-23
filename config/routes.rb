55caa04d70a410ec65c142e82e0c30ec8c7461df
Angel Shamieh <angellshamieh5@gmail.com>
2020-02-23 15:34:11 +0200

    remove typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draanimw do
+Rails.application.routes do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
