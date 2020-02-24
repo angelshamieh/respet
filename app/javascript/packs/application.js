1e9b2fead2d79b80e7c6500ab1211d7723fbf6a2
Charlotte Lui <charlotteytlui@gmail.com>
2020-02-24 20:24:18 +0200

    add highlight when select

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,8 @@
 import "bootstrap";
 import { initFilter } from "../components/animal-list-filter"
 import { initGenderFilter } from "../components/adoption_gender_list"
+import { initNavHighlight } from "../components/navbar_highlight"
 
-
-initFilter()
-initGenderFilter()
+initFilter();
+initGenderFilter();
+initNavHighlight();
