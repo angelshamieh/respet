edcc3b3bd09cf61e90ef02b3fde1b3eef8f56619
Charlotte Lui <charlotteytlui@gmail.com>
2020-02-25 14:21:17 +0200

    set up mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,11 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { initFilter } from "../components/animal-list-filter"
 import { initGenderFilter } from "../components/adoption_gender_list"
 import { initNavHighlight } from "../components/navbar_highlight"
+import { initMapbox } from '../plugins/init_mapbox';
 
 initFilter();
 initGenderFilter();
 initNavHighlight();
+initMapbox();
