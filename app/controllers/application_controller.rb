9fa45bb16f9ca0eefa34cc6d310882627915efa2
Charlotte Lui <charlotteytlui@gmail.com>
2020-02-25 14:04:58 +0200

    updated route, nav link, g controller for medical centers and install mapbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^dashboard$)/
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(^dashboard$)|(^medical_centers$)/
   end
 
   protected
---
+++ app/controllers/medical_centers_controller.rb
@@ -0,0 +1,14 @@
+class MedicalCentersController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    @centers = MedicalCenter.geocoded
+
+    @markers = @centers.map do |center|
+      {
+        lat: center.latitude,
+        lng: center.longitude
+      }
+    end
+  end
+end
