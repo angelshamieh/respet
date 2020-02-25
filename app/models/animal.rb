31eaa6683f3697d0c5fd318a97c91d2cd1b2279f
talia wolf <taliamwolf@gmail.com>
2020-02-25 17:49:09 +0200

    must upload photo when creating an animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,11 +4,13 @@ class Animal < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
+  validates :photos, presence: true
   validates :location, presence: true
   validates :description, presence: true
   validates :gender, inclusion: { in: %w(male female), message: "must be male or female"}
 
   has_many_attached :photos
+
 
   def self.search_by_status_status(status)
     Animal.joins(:animal_status).where('animal_statuses.status = ?', status)
