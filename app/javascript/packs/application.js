01da86b0c62d1623ef173b1ab05cad8ae7194a6b
Charlotte Lui <charlotteytlui@gmail.com>
2020-02-25 15:24:56 +0200

    add search bar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initFilter } from "../components/animal-list-filter"
 import { initGenderFilter } from "../components/adoption_gender_list"
 import { initNavHighlight } from "../components/navbar_highlight"
