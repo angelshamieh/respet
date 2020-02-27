5620225180387aaf7ebbb4452a3a55207d3b4ade
talia wolf <taliamwolf@gmail.com>
2020-02-27 11:43:10 +0200

    added bookmard model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :messages
   has_many :animals, dependent: :destroy
+  has_many :bookmarks
 
   validates :first_name, presence: true
   validates :last_name, presence: true
